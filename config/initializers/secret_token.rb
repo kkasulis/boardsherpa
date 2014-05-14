52c578492f9e32c199218f19dc02f0ce62a760a7
Kyle Kasulis <ktkasulis@gmail.com>
2014-05-13 23:04:13 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Boardsherpa::Application.config.secret_key_base = '1a80bd64483cf2b4427313669c3a9c7d87596f577d83ce30a28fa0922db97de4c6b71f4d543468bd87e09b15ab7c08b191791f3ac52d9b967b7d512068231bb2'
